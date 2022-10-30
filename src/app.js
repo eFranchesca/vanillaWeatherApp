d21b431c62351413042060a4f24cc3d35a2de8f7
eFranchesca <112275326+eFranchesca@users.noreply.github.com>
2022-10-29 23:40:01 -0400

    modified js & html
    wind speed, temp, and conditions are real data

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 function displayTemperature(response) {
- let temperatureElement = document.querySelector("#temperature");
+  console.log(response.data);
+ let temperatureElement = document.querySelector("#temperature");  
+ let cityElement = document.querySelector("#city");
+ let descriptionElement = document.querySelector("#description");
+ let windElement = document.querySelector("#wind");
+ 
  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+ windElement.innerHTML = Math.round(response.data.wind.speed);
+
 }
 
 let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
