eed8987038aa42c965638ee75871642ba0941750
eFranchesca <112275326+eFranchesca@users.noreply.github.com>
2022-10-31 10:58:10 -0400

    modified id's, added to js
    trying to get feelsLike and date to work

--- src/app.js
+++ src/app.js
@@ -1,14 +1,50 @@
+function formatDate(timestamp) {
+ let date = new Date(timestamp);
+ let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+ let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+ let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+ ];
+ let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayTemperature(response) {
   console.log(response.data);
  let temperatureElement = document.querySelector("#temperature");  
  let cityElement = document.querySelector("#city");
  let descriptionElement = document.querySelector("#description");
  let windElement = document.querySelector("#wind");
- 
- temperatureElement.innerHTML = Math.round(response.data.main.temp);
-  descriptionElement.innerHTML = response.data.weather[0].description;
- windElement.innerHTML = Math.round(response.data.wind.speed);
+ let dateElement = document.querySelector("#time");
+ let feelsLikeElement = document.querySelector("#feelsLike");
 
+ temperatureElement.innerHTML = Math.round(response.data.main.temp);
+ descriptionElement.innerHTML = response.data.weather[0].description;
+ windElement.innerHTML = Math.round(response.data.wind.speed);
+ dateElement = formatDate(response.data.dt * 1000);
+ feelsLikeElement = response.data.main.feels_like;
+  console.log(response.data.main.feels_like);
 }
 
 let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
