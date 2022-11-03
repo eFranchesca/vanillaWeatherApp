85ff3156042def3ebaebaf86a7bb9bb8043eca74
eFranchesca <112275326+eFranchesca@users.noreply.github.com>
2022-11-03 12:54:56 -0400

    added to js
    added getForcast, fixed duplicate id=city, still having issues with icon and search

--- src/app.js
+++ src/app.js
@@ -22,6 +22,13 @@ function formatDate(timestamp) {
 return `${day} ${hours}:${minutes}`;
 }
 
+function getForcast(coordinatesButton) {
+  let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+  console.log(coordinates.lat);
+}
+
 function displayTemperature(response) {
  let cityElement = document.querySelector("#city");
  let dateElement = document.querySelector("#date");
@@ -31,18 +38,34 @@ function displayTemperature(response) {
  let feelsLikeElement = document.querySelector("#feelsLike");
  let windElement = document.querySelector("#wind"); 
 
- 
+ cityElement.innerHTML = response.data.name;
  dateElement.innerHTML = formatDate(response.data.dt * 1000);
  temperatureElement.innerHTML = Math.round(response.data.main.temp);
  descriptionElement.innerHTML = response.data.weather[0].description;
  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
  windElement.innerHTML = Math.round(response.data.wind.speed);
- iconElement.setAttribute("src",
-  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-iconElement.setAttribute("alt", response.data.weather[0].description);
+ iconElement.setAttribute(
+  "src",
+  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+ console.log(response.data.weather[0].icon) 
+}
+
+function search(city) {
+  let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Fort Lauderdale&appid=${apiKey}&units=imperial`;
+  
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+ function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
  }
 
-let apiKey = "7d478f69e1b2f5d563653f13f5f91d76";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Fort Lauderdale&appid=${apiKey}&units=imperial`;
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
 
-axios.get(apiUrl).then(displayTemperature);
+search("Fort Lauderdale");
